c769c06daf967ab4606f9d9c77b56c28ce01ce31
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-01-29 20:15:07 +0600

    added app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+var name = 'swadeb';
+var num1= 56;
+var num2= '25.55';
+var friends =['rohim', 'Korim', 'Utpaul', 'Janto'];
+var MidwifeInfo ={
+    name: 'Maya',
+    Age: 27,
+    Adress: 'Ukhiya',
+    phNumber: 01761073247,
+}
+
+
+/* Type OF Variable*/ 
+// console.log(typeof name);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof friends);
+// console.log(typeof MidwifeInfo);
+
+/*
+ParseInt and ParseFloat
+*/ 
+var num1 = 20;
+var num2 = '10';
+var num3 ='30.6663244';
+
+var conNum3 = parseFloat(num3);
+var sum = num1+ conNum3;
+var threeNum = sum.toFixed(3);
+var convertSum = parseFloat(threeNum)
+// console.log(convertSum)
+
+var perseNum = parseInt(num2);
+var total = num1 +perseNum;
+
+// console.log(sum);
